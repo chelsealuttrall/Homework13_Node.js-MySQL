9cee366a0880b9848b6a00e83517271e28a3a124
chelsealuttrall <chelsealuttrall@gmail.com>
2020-04-29 16:38:33 -0600

    IT WORKS!

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,47 +10,37 @@ module.exports = function (app) {
     res.json(friends);
   });
   app.post("/api/friends", function (req, res) {
-    userData = req.body;
-    let newRider = userData;
+    let newRider = req.body;
+    console.log("newRider.scores", newRider.scores);
 
-    console.log("all da friends:", friends);
-    // Displays all characters
-
-    console.log("NewRider:", newRider);
-    let bestScore = 51;
-    let bestFriend = "";
-
-    // Convert each user 's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`). *
-    for (let j = 0; j < friends; j++) {
-      let friend = friends[j];
-      console.log(friend);
-      friend = friend[j + 1];
-      diffArray = [];
-      let diffArray = [];
-        for (let q = 0; q < friend.scores.length; q++) {
-          let rAnswer = friend.scores[q];
-          let newRAnswer = newRider.scores[q];
-          rAnswer, newRAnswer;
-          let racerQuestionDifference = rAnswers - newRAnswers;
-          //subtract the differences between the answers.
-          //push that absolute number to the diffArray.
-          Math.abs(racerQuestionDifference).push(diffArray);
-          q++;
-        }
-        //then we consolidate the total differences.
-        diffArray.reduce((a, b) => a + b, 0);
-        console.log(diffArray);
-        let comparisonScore = diffArray;
-        if (comparisonScore < bestScore) {
-          bestFriend = friend[j].name;
-          friend[j].push(diffArray.val());
-        }
+    for (let i = 0; i < newRider.scores.length; i++) {
+      newRider.scores[i] = parseInt(newRider.scores[i]);
     }
 
-    let data = { bestFriend: bestFriend, bestScore: bestScore };
+    let diffArray = [50];
+    let riderBud = {};
+    console.log("diffArray", diffArray, "riderBud", riderBud);
+    for (let i = 0; i < friends.length; i++) {
+      let compatibleRider = friends[i];
+      let totalDiff = 0;
+
+      for (let j = 0; j < compatibleRider.scores.length; j++) {
+        let thisDiff = Math.abs(compatibleRider.scores[j] - newRider.scores[j]);
+        totalDiff += thisDiff;
+        console.log("totalDiff += thisDiff", (totalDiff += thisDiff));
+        riderIndex = compatibleRider;
+      }
+      console.log("diffArray", diffArray, "riderBud", riderBud);
+      if (totalDiff < diffArray) {
+        diffArray = [totalDiff];
+        riderBud = riderIndex;
+      }
+    }
+
     friends.push(newRider);
-    res.json(data);
-    console.log("Your riding mojo score most closely resembles ", data, "'s.");
-    console.log(friends);
+    res.json(riderBud.name);
+    console.log("Your riding mojo score most closely resembles ", riderBud.name, "'s.");
+    //   console.log(friends);
+    //
   });
 };
